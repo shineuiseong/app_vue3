59d2707d7702c8889fa3322f5b0c1f2132386dcd
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2022-06-04 15:46:12 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components:{
+    
+  }
+
+}
+</script>
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
